1dfa56c9f33839e699c7fce3c70e52a90d91b593
larryzhao <larrylzhao@gmail.com>
2018-06-18 23:38:55 -0500

    updates

---
+++ my_stuff/ch1/07.py
@@ -0,0 +1,31 @@
+import unittest
+
+def rotate_matrix_in_place(m):
+    n = len(m)
+    for col in range(n/2):
+        for row in range(col, n - col - 1):
+            top = m[row][col]
+            m[row][col] = m[col][n - row - 1]
+            m[col][n - row - 1] = m[n - row - 1][n - col - 1]
+            m[n - row - 1][n - col - 1] = m[n - col - 1][row]
+            m[n - col - 1][row] = top
+
+class Test(unittest.TestCase):
+
+
+    def test_rotate_matrix_in_place(self):
+        mat1 = [[1,2],[3,4]]
+        mat2 = [[2,4],[1,3]]
+        rotate_matrix_in_place(mat1)
+        self.assertEqual(mat1, mat2)
+        mat3 = [[1,2,3],[4,5,6],[7,8,9]]
+        mat4 = [[3,6,9],[2,5,8],[1,4,7]]
+        rotate_matrix_in_place(mat3)
+        self.assertEqual(mat3, mat4)
+        mat5 = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+        mat6 = [[4,8,12,16],[3,7,11,15],[2,6,10,14],[1,5,9,13]]
+        rotate_matrix_in_place(mat5)
+        self.assertEqual(mat5, mat6)
+
+if __name__ == "__main__":
+    unittest.main()
